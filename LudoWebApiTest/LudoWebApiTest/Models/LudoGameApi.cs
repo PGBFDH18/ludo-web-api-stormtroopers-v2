a98b1893f7876bf3f4b8aca53ce12ef67219a72d
Wael Hasan <wael850224@gmail.com>
2019-01-27 13:19:12 +0100

    te

---
+++ LudoWebApiTest/LudoWebApiTest/Models/LudoGameApi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LudoWebApiTest.Models
+{
+    public class LudoGameApi
+    {
+        public string StateOfTheGame { get;  set;}
+        
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Projekt formulering
-[Fia Web API](https://pgbfdh18.github.io/webbutveckling-backend/project2.html)
