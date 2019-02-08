27183e91596a0b38d0e54495dcffe2cbdfd00c75
Wael Hasan <wael850224@gmail.com>
2019-02-08 08:33:01 +0100

    Console App Added

---
+++ src/LudoConsole/ConsoleApp13/Program.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var client = new RestClient("http://ludoapp.azurewebsites.net/ludo");
+
+           var request = new RestRequest("getallgames", Method.GET);
+           IRestResponse response = client.Execute(request);
+           var content = response.Content; // raw content as string
+           Console.WriteLine(content);
+                        Console.ReadKey();
+        }
+    }
+}
